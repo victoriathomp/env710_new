fc1a8245c242da291a6afb661302b9831bcb004e
camirodr <c.rodriguez@duke.edu>
2025-03-30 17:36:15 -0400

    abundance data + model #1 -Camila

--- Metadata_biodiversity.R
+++ Metadata_biodiversity.R
@@ -6,11 +6,11 @@
 #1 Fish/salamander
 #2  invertebrate
 #3 Stream/ stream transect data
-#4
+#4 wood measurements 
 #5 Pool measurement
 #More background info:
   
-Description:
+#Description:
 #The study was conducted within nine reach-pair sites located within the McKenzie River Basin 
 #in the western Cascade Mountains of Oregon. Seven of the sites were located within the HJ Andrews 
 #Experimental Forest (HJA), a 6,400 ha research forest encompassing the entire Lookout Creek drainage
@@ -42,4 +42,35 @@ Description:
 #riparian forests, this design is similar to a before-after control-impact study with riparian regeneration
 #as the treatment. The same study reaches were sampled and evaluated how 40 years of riparian regeneration
 #influenced our response variables.
+  
+  
+#SITE DESCRIPTION:
+#The previously harvested cutblocks were cleared 40-60 years prior to this study. In all cases,
+#timber was removed down to the stream bank with no riparian buffer. Trees were replanted 
+#within 5 years post-harvest in seven of the nine cuts in accordance with forest management 
+#practices at the time. Stands in MCTW and Mack Creek were regenerated without any post-harvest
+#planting. In 2014, the second growth riparian forests were predominantly Douglas fir (Pseudotsuga 
+#menziesii) but red alder (Alnus rubra) was also a common canopy species in areas directly adjacent 
+#to streams and provided substantial stream shading. Old-growth forests were comprised of Douglas fir,
+#western hemlock (Tsuga heterophylla) and western red cedar (Thuja plicata). Red alder was present 
+#adjacent to streams within old-growth forests as well, but it was not as common as in second 
+#growth sections.
+
+#Coastal cutthroat trout and coastal giant salamanders (Dicamptodon tenebrosus) were present in
+#all 18 stream reaches and were the dominant vertebrates. Sculpin (Cottus spp.) were present in
+#both reaches of MR404, but were not found in any other reaches. Tailed frogs (Ascaphis truei) 
+#were found in low abundance in some of the streams, but were not evaluated in this study.
+
+
+#Description of distinction between old-growth and second growth study reaches:
+#COOK - Cook Creek, a tributary of Blue River upstream of Blue River reservoir and north of 
+#the HJ Andrews 
+#FRITZ - Fritz Creek, a tributary of Deer Creek, which flows directly into the McKenzie River, NE of the HJ Andrews
+#LO701 - Harvest block LO701 on Lookout Creek. Second growth section was in cutblock of LO701. The downstream end of this reach was 180 m upstream of the cutbreak. The old-growth section was downstream of cut LO701. The upstream end was 75 m downstream from the cutbreak.
+#LO703 - Harvest block LO703 on Lookout Creek. Second growth section was in cutblock of LO703. The downstream end of this reach was 85 m upstream of the western edge of cutbreak LO703. The old-growth section was upstream of cut LO703. The downstream end of the reach was 50 m upstream from the eastern edge of cutbreak LO703.
+#MACK - Mack Creek. This site is part of an LTER fish study.
+#MCT_E - McRae Creek east tributary . Second growth section was in cutblock of L504. The old-growth reach was upstream of the second growth reach here. The upstream end of the second growth reach was 200 m downstream of the cutbreak. The downstream end of the old-growth section was 35 m upstream of the cutbreak.
+#MCT_W - McRae Creek west tributary. Second growth section was in cutblock of L503. The downstream end of the second growth reach was 90 m upstream of the cutbreak. The upstream end of the old-growth section was 50 m downstream of the cutbreak.
+#MR404 - Harvest block L404 on McRae Creek. Second growth section was in cutblock of L404. The downstream end of the second growth reach was 110 m upstream of the cutbreak. The upstream end of the old-growth section was 80 m downstream of the cutbreak.
+#STR - Harvest block L504 on McRae Creek. Second growth section was in cutblock of L504. The old-growth reach was upstream of the second growth reach here. The upstream end of the second growth reach was 75 m downstream of the cutbreak. The downstream end of the old-growth section was 75 m upstream of the cutbreak.
 
