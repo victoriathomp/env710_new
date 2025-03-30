81c8354c41c49494fce066fb872366c2c1a53c8c
camirodr <c.rodriguez@duke.edu>
2025-03-30 16:41:00 -0400

    Updates to code 03/30- Camila

--- Metadata_biodiversity.R
+++ Metadata_biodiversity.R
@@ -1,1 +1,37 @@
 # Link for metadata: https://andlter.forestry.oregonstate.edu/data/abstract.aspx?dbcode=AS010
+
+#More background info:
+  
+Description:
+#The study was conducted within nine reach-pair sites located within the McKenzie River Basin 
+#in the western Cascade Mountains of Oregon. Seven of the sites were located within the HJ Andrews 
+#Experimental Forest (HJA), a 6,400 ha research forest encompassing the entire Lookout Creek drainage
+#basin. Cook Creek and Fritz Creek were located outside of the HJA. Cook Creek is a tributary of Blue
+#River upstream of Blue River reservoir and Fritz Creek is a tributary of Deer Creek, which flows 
+#directly into the McKenzie River.
+
+#Each pair consisted of two reaches: one within a section of stream with old-growth riparian 
+#forest, and another in a nearby section of stream bordered by second-growth riparian forest on 
+#at least one stream bank. Harvesting in the previously managed reaches occurred on just one stream 
+#bank in three reach pairs (MR404, LO701, and LO703) and on both banks for all other pairs. Sites 
+#were selected based on the presence of old-growth and second growth riparian forests close in 
+#proximity on the same stream (within 500 m). Having distinctly different forest types along two 
+#nearby sections of the same stream reduces inherent stream-to-stream environmental variability 
+#(e.g. temperature, gradient, geology, substrate, etc.) that often arises in comparisons between 
+#whole-stream systems in basins with managed versus unmanaged, late succession forests. Reaches 
+#ranged from 90-200 m and reaches within a reach pair were separated by a 90-325 m buffer section.
+
+#Metrics of habitat and productivity relative to fish and salamander biomass in nine stream reach 
+#pairs were collected. Considering differences among streams and between reaches within each stream, 
+#relationships between both biotic and abiotic covariates and the biomass of coastal cutthroat trout 
+#(Onchorhynchus clarkii clarkii), coastal giant salamanders (Dicamptodon tenebrosus) and total vertebrates
+#(fish and salamanders) were evaluated.
+
+#Five of the six fish-bearing stream reach pairs originally surveyed by Murphy and Hall (1981) were 
+#visited to determine how stream conditions, benthic biofilms, invertebrate predators, and ultimately 
+#resident coastal cutthroat trout have responded to nearly four decades of riparian forest regeneration.
+#Using the upstream reference reaches identified by Murphy (1979), which were bordered by old-growth 
+#riparian forests, this design is similar to a before-after control-impact study with riparian regeneration
+#as the treatment. The same study reaches were sampled and evaluated how 40 years of riparian regeneration
+#influenced our response variables.
+
